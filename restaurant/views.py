03dc5edc45077883ac53e6712238b0cde894ac73
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-02-29 18:45:00 +0200

    added menu and category views

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import status, generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Menu, Category
+from .serializers import MenuSerializer, CategorySerializer
+
+
+class MenuListView(generics.ListAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class DishDetailView(generics.RetrieveAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    lookup_field = 'id'
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryMenuListView(generics.ListAPIView):
+    serializer_class = MenuSerializer
+
+    def get_queryset(self):
+        category_id = self.kwargs['category_id']
+        return Menu.objects.filter(categories__id=category_id)
