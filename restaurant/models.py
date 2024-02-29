a374a2642f741375b0c73a338167ee645c5cf2d4
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-02-29 18:43:29 +0200

    create models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from datetime import datetime, timedelta
 
-# Create your models here.
+from django.utils.timezone import make_aware
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Menu"
+
+
+class Reservation(models.Model):
+    guests = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    date = models.DateField()
+    time = models.TimeField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    special_request = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
