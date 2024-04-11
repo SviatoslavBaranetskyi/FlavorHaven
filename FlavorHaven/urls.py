4ece397dad85f906433387ccbd4bb2b9e233faba
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-04-11 12:50:57 +0300

    added Swagger documentation for API endpoints

--- FlavorHaven/urls.py
+++ FlavorHaven/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from restaurant.yasg import urlpatterns as doc_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('restaurant.urls')),
 ]
+
+urlpatterns += doc_urls
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,6 @@
 from django.utils import timezone
-from django.http import JsonResponse
-from rest_framework import status, generics
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
+from drf_yasg.utils import swagger_auto_schema
 
 from .models import Menu, Category, Reservation
 from .serializers import MenuSerializer, CategorySerializer, ReservationSerializer
@@ -12,21 +10,28 @@ class MenuListView(generics.ListAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
+    @swagger_auto_schema(operation_summary="Get list of all menus")
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
 
 class DishDetailView(generics.RetrieveAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     lookup_field = 'id'
 
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+    @swagger_auto_schema(operation_summary="Get details of a dish by ID")
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
 
 class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    @swagger_auto_schema(operation_summary="Get list of all categories")
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
 
 class CategoryMenuListView(generics.ListAPIView):
@@ -35,6 +40,10 @@ class CategoryMenuListView(generics.ListAPIView):
     def get_queryset(self):
         category_id = self.kwargs['category_id']
         return Menu.objects.filter(categories__id=category_id)
+
+    @swagger_auto_schema(operation_summary="Get list of dishes by category ID")
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
 
 class ReservationListView(generics.ListCreateAPIView):
@@ -47,3 +56,11 @@ class ReservationListView(generics.ListCreateAPIView):
 
         # Возвращаем актуальный queryset
         return Reservation.objects.all()
+
+    @swagger_auto_schema(operation_summary="Get list of reservations")
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_summary="Create a new reservation")
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
